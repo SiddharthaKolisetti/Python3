c8729dce6dbcc6740f0ae24eee17e51113f97db1
SiddharthaKolisetti <siddharthakolisetti@gmail.com>
2020-06-15 00:24:53 +0530

    Optimized code for prime number

--- 4primenumber.py
+++ 4primenumber.py
@@ -1,10 +1,10 @@
 def num(x):
-    for i in range(2,x+1):
-        if x%i==0 and i!=x:
+    for i in range(2,int(x/2)+2):
+        if x%i==0 :
             return False
             break
         #print(i)
-        if i==x:
+        if i==int(x/2)+1:
             return True
 
-print(num(2))
+print(num(93))
